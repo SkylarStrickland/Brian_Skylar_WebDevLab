d47eb609fe7ce5bd0d85afc9bb6d96715b0f0f3c
btejano <btejano1606@gmail.com>
2024-11-24 20:48:37 -0500

    Add files via upload

---
+++ 2_Bryan_info.py
@@ -0,0 +1,88 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "Images/profile.jpeg"
+about_me = "My name is Bryan Tejano, a first year Physics major at Georgia Tech. I am super passionate about astrophysics and computational physics!"
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/bryan-tejano/"
+my_github_url = "https://github.com/btejano"
+my_email_address = "btejano1606@gmail.com"
+
+
+education_data ={
+    'Degree': 'Bachelor of Science in Physics',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'May 2028',
+    'GPA': '3.5'
+}
+
+course_data = {
+    "code":["MATH 1554", "MATH 2551", "CHIN 1001", "MATH 2552", "GT 1000", "PHYS 2213", "CS 1301", "APPH 1040"], 
+    "names":["Linear Algebra", "Multivariable Calculus", "Elementary Chinese I", "Differential Equations", "Freshman Seminar", "Intro to Modern Physics", "Intro to Computer Science", "Scientific Foundations of Health"], 
+    "semester_taken":["Dual Enrollment", "Dual Enrollment", "1st", "1st", "1st", "1st", "1st", "1st"],
+    "skills":["Eigenvalues are important!", "Spherical coordinates are not fun", "我是大学生！", "Solving 1st and 2nd order differental equations!", "All the career opportunities as a physics major!", "Quantum mechanics is crazy!", "Yay CS!", "Staying healthy!"],
+    }
+experience_data = {
+    "Production Department Intern at Motovario Inc.": (["Worked from August 2023 to August 2024.",
+                                                            "- Assembled industrial transmissions and gearboxes for wastewater treatment and mechanical-electromechanical industries.",
+                                                            "- Ensured quantity of stock material by overviewing job orders to create an efficient production line.", 
+                                                            "- Researched and gained insight on applications of engineering and physics concepts in the manufacturing industry."],
+                                                            "Images/motovario.jpg"),
+    "Cashier and Barista at Pearl's Tea":(["Worked from May 2022 to August 2023.",
+                                    "- Provided efficient and friendly customer service to 100+ guests per shift.",
+                                    "- Recommended unique drink and topping combinations to increase the demand of less popular items.",
+                                    "- Assisted managers when training new employees to run the cashier and make drinks to promote efficiency."],
+                                    "Images/pearls.jpg"),
+}
+
+projects_data = {
+    "FCHS STEM Senior Capstone": "Researched and applied mathematical concepts of polar curves to enhance concepts in music theory. Analyzed patterns across 30 different chords and 4 chord progressions in popular pop music. Learned basic Python skills to project chord progressions in an accessible way."
+}
+
+programming_data = {
+    "Python": 90,
+    "Java Script": 30,
+    "C": 0,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "Java Script": "☕",
+    "C": "🔍",
+}
+spoken_icons = {
+    "English": "🇺🇸",
+    "Chinese":"🇨🇳"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "Chinese": "Beginner"
+}
+leadership_data = {
+    "Co-President of FCHS Technology Student Association": (["- Led monthly chapter meetings toward 150+ members to increase club engagement.",
+                                                             "- Organized Google Sheets in preparation of bringing 60+ members to a state-wide conference.",
+                                                             "- Coordinated with local high schools in the county to organize county-wide events for our chapters.",
+                                                             "- Mentored multiple competitions throughout the school year in preparation for the state-wide and national conferences.",
+                                                             "- Created a website for our TSA chapter to help consolidate information regarding competitions and important updates."],
+                                                             "Images/tsa.jpg"),
+    "VP of Fundraising of FCHS Technology Student Association": (["- Organized and raised $200 selling laser-cut Christmas wreaths to raise money for club trips.",
+                                                                  "- Re-established the VP of Fundraising role by updating the offer bylaws and responsibilities as well as reviving connections with our school's administrative team.",
+                                                                  "- Mentored multiple competitions throughout the school year in preparation for the state-wide and national conferences."],
+                                                                  "Images/tsa2.jpg")
+}
+activity_data={
+    "FCHS Ambassadors": ["- Volunteered as a tour guide for rising freshman in highschool", 
+            "- Facilitated school wide and county wide events"]
+}
