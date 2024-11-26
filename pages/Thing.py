20e15ad0ef755299cbaec0adc62e6ad259aa3478
btejano <btejano1606@gmail.com>
2024-11-26 15:12:43 -0500

    Update Thing.py

--- pages/Thing.py
+++ pages/Thing.py
@@ -7,10 +7,12 @@ genai.configure(api_key="AIzaSyB45quDtyWzRw_ErsU-fxsv_kmytrHLyNM")
 model = genai.GenerativeModel("gemini-1.5-flash")
 st.write("Write down a character!")
 character = st.text_input("") # NEW 
-response = model.generate_content(f"give me background of {character}")
+inputRequest = f"give me background of {character}"
+response = model.generate_content(inputRequest)
 
 if character == "":
   st.write("")
 else:
+  st.write(inputRequest)
   st.write(response.text)
 
