9f463565f2744d7186b48b446a8ba247d6be1b2b
btejano <btejano1606@gmail.com>
2024-11-26 14:32:13 -0500

    Update Thing.py

--- pages/Thing.py
+++ pages/Thing.py
@@ -8,7 +8,7 @@ genai.configure(api_key="AIzaSyB45quDtyWzRw_ErsU-fxsv_kmytrHLyNM")
 
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content("hi")
-st.write(response)
+st.write(response.text)
 
 
 
