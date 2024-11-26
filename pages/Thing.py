f6c2280b4ae320caf771411fb2a079c15d8678c4
SkylarStrickland <skylarpstrickland@gmail.com>
2024-11-26 14:25:52 -0500

    Update Thing.py

--- pages/Thing.py
+++ pages/Thing.py
@@ -3,11 +3,7 @@ import os
 genai.configure(api_key="AIzaSyB45quDtyWzRw_ErsU-fxsv_kmytrHLyNM")
 
 
-'''
-key = st.secrets["key"]
 
-genai.configure(api_key=key)
-'''
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content("hi")
 print(response.text)
