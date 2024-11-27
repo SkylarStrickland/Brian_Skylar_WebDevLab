ffa465f82676490029b718c0c31ae398e0774041
btejano <btejano1606@gmail.com>
2024-11-26 20:35:22 -0500

    Update Home_Page.py

--- Home_Page.py
+++ Home_Page.py
@@ -21,7 +21,7 @@ st.subheader("Bryan Tejano, Skylar Strickland")
 st.write("""
 Welcome to our Streamlit Web Development Lab03 app! You can navigate between the pages using the sidebar to the left. The following pages are:
 
-1. Brian Portfolio: An online portfolio of Brian's professional experience
+1. Bryan Portfolio: An online portfolio of Bryan's professional experience
 2. Skylar Portfolio: An online portfolio of Skylar's professional experience
 3. Suitable Disney Character: Find a Disney character from a movie based on your imagination
 4. Disney Show Character Explorer: Find a Disney character from a show, learn all about them, and ask any questions
